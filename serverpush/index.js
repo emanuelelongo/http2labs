c60edb16257196796d5b64591b83e68c692ce958
Emanuele Longo <longo.emanuele@gmail.com>
2016-11-11 00:25:43 +0100

    try server push

---
+++ serverpush/index.js
@@ -0,0 +1,23 @@
+let spdy = require('spdy'),
+    fs = require('fs');
+
+let options = {
+    key: fs.readFileSync(__dirname + '/server.key'),
+    cert:  fs.readFileSync(__dirname + '/server.crt')
+};
+
+spdy.createServer(options, function(req, res) {
+    let stream = res
+        .push('/main.js', {
+            request: {
+                accept: '*/\*'
+            },
+            response: {
+                'content-type': 'application/javascript'
+            }
+        })
+        .end('console.log("Hello World");');
+    
+    res.writeHead(200);
+    res.end('<script src="/main.js"></script>');
+}).listen(3000);
