dd835fd96d64649f94d315132fa369e9976bb65c
diblasig <giovanni.diblasi@ynap.com>
2016-11-14 11:15:23 +0100

    push more resources

--- serverpush/index.js
+++ serverpush/index.js
@@ -7,7 +7,17 @@ let options = {
 };
 
 spdy.createServer(options, function(req, res) {
-    let stream = res
+     console.log(req.url);
+    if(req.url=='/'){
+        pushScript(res);
+    }
+     if(req.url=='/pushimage'){
+        pushImage(res);
+    }
+}).listen(3000);
+
+function pushScript(res){
+ let stream = res
         .push('/main.js', {
             request: {
                 accept: '*/\*'
@@ -16,8 +26,39 @@ spdy.createServer(options, function(req, res) {
                 'content-type': 'application/javascript'
             }
         })
+        
         .end('console.log("Hello World");');
     
     res.writeHead(200);
     res.end('<script src="/main.js"></script>');
-}).listen(3000);
+};
+
+
+function pushImage(res){
+   
+ let stream = res
+        .push('/push.png', {
+            request: {
+                accept: '*/\*'
+            },
+            response: {
+                'content-type': 'image/jpeg'
+            }
+        })
+         .end(fs.readFileSync(__dirname + '/push.png'));
+      
+    let stream2 = res
+        .push('/pushok.png', {
+            request: {
+                accept: '*/\*'
+            },
+            response: {
+                'content-type': 'image/jpeg'
+            }
+        })
+         .end(fs.readFileSync(__dirname + '/pushgreen.png'));
+    
+    res.writeHead(200);
+    res.end('<html><body><img src="/push.png" height="100" width="100"></body></html>');
+  
+};
